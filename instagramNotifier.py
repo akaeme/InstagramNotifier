4b523db3ac7a99a546d0892e8436fd272948b9d1
akaeme <oibaf3@live.com.pt>
2017-06-26 23:32:01 +0100

    instagram api

--- instagramNotifier.py
+++ instagramNotifier.py
@@ -1,28 +1,21 @@
-from instagram.client import InstagramAPI
+from InstagramAPI import InstagramAPI
 
 
 def run():
-    #access_token = input('Access Token: ')
-    #client_secret = input('Client Secret: ')
+    InstagramAPI = InstagramAPI("akaemee", "napster3680fabiosilva")
+    InstagramAPI.login()  # login
 
-    access_token = '255290884.7729a55.cdbbd915e561453fa81f2c1c8b91fb54'
-    client_secret = '57a594edb6e8495cb366ae42acd60f18'
+    # InstagramAPI.tagFeed("flowers") # get media list by tag #cat
+    # media_id = InstagramAPI.LastJson # last response JSON
+    # InstagramAPI.like(media_id["ranked_items"][0]["pk"]) # like first media
 
-    api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-    recent_media, next_ = api.user_recent_media(user_id='255290884')
-    photos = []
-    print(len(recent_media))
-    for media in recent_media:
-        print(media.id)
-        photos.append('<img src="%s"/>' % media.images['thumbnail'].url)
+    InstagramAPI.getSelfUserFollowers()
+    # last response JSON
+    media_id = InstagramAPI.LastJson
+    print(media_id)
 
-    follows, next_ = api.user_follows()
-    while next_:
-        more_follows, next_ = api.user_follows(with_next_url=next_)
-        follows.extend(more_follows)
-
-
-    str='https://api.instagram.com/v1/media/1314630905147227181_255290884/likes?access_token=255290884.7729a55.cdbbd915e561453fa81f2c1c8b91fb54'
+    # print(type(InstagramAPI.getUserFollowers(media_id["ranked_items"][0]["user"]["pk"]))) # get first media owner
+    # followers
 
 if __name__ == "__main__":
     run()
